7372f5c8535cf35b5f30dcc0d02e2a3560ff8e83
ByAlexxX <92841381+ByAlexxX@users.noreply.github.com>
2021-10-22 15:06:01 -0500

    Update imagemaker.js

--- database/menu/imagemaker.js
+++ database/menu/imagemaker.js
@@ -6,32 +6,30 @@ const imagemaker = (prefix, botName, ownerName) => {
   ❏ Prefix: 「  ${prefix}  」
   ❏ Creator: ${ownerName}
 ◪ *ABOUT*
-  │
-  ├─ ❏ ${prefix}info
-  ├─ ❏ ${prefix}blocklist
-  ├─ ❏ ${prefix}chatlist
-  ├─ ❏ ${prefix}ping
-  └─ ❏ ${prefix}bugreport
+  • ❏ ${prefix}info
+  • ❏ ${prefix}blocklist
+  • ❏ ${prefix}chatlist
+  • ❏ ${prefix}ping
+  • ❏ ${prefix}bugreport
 ◪ *IMAGE MAKER*
-  │
-  ├─ ❏ ${prefix}bpink
-  ├─ ❏ ${prefix}snowwrite
-  ├─ ❏ ${prefix}3dtext
-  ├─ ❏ ${prefix}firetext
-  ├─ ❏ ${prefix}glitch
-  ├─ ❏ ${prefix}shadow
-  ├─ ❏ ${prefix}burnpaper
-  ├─ ❏ ${prefix}coffee
-  ├─ ❏ ${prefix}lovepaper
-  ├─ ❏ ${prefix}woodblock
-  ├─ ❏ ${prefix}qowheart
-  ├─ ❏ ${prefix}mutgrass
-  ├─ ❏ ${prefix}undergocean
-  ├─ ❏ ${prefix}woodenboards
-  ├─ ❏ ${prefix}wolfmetal
-  ├─ ❏ ${prefix}metalictglow
-  ├─ ❏ ${prefix}8bit
-  ├─ ❏ ${prefix}herrypotter
-  └─ ❏ ${prefix}quotemaker`
+  • ❏ ${prefix}bpink
+  • ❏ ${prefix}snowwrite
+  • ❏ ${prefix}3dtext
+  • ❏ ${prefix}firetext
+  • ❏ ${prefix}glitch
+  • ❏ ${prefix}shadow
+  • ❏ ${prefix}burnpaper
+  • ❏ ${prefix}coffee
+  • ❏ ${prefix}lovepaper
+  • ❏ ${prefix}woodblock
+  • ❏ ${prefix}qowheart
+  • ❏ ${prefix}mutgrass
+  • ❏ ${prefix}undergocean
+  • ❏ ${prefix}woodenboards
+  • ❏ ${prefix}wolfmetal
+  • ❏ ${prefix}metalictglow
+  • ❏ ${prefix}8bit
+  • ❏ ${prefix}herrypotter
+  • ❏ ${prefix}quotemaker`
 }
 exports.imagemaker = imagemaker
