9006cd5a7cd616c3d59da5edb404a3c18b1f486d
ByAlexxX <92841381+ByAlexxX@users.noreply.github.com>
2021-10-22 15:09:02 -0500

    Update group.js

--- database/menu/group.js
+++ database/menu/group.js
@@ -6,34 +6,32 @@ const group = (prefix, ownerBot, botName) => {
   ❏ Prefix: 「  ${prefix}  」
   ❏ Creator: ${ownerBot}
 ◪ *ABOUT*
-  │
-  ├─ ❏ ${prefix}info
-  ├─ ❏ ${prefix}blocklist
-  ├─ ❏ ${prefix}chatlist
-  ├─ ❏ ${prefix}ping
-  └─ ❏ ${prefix}bugreport
+  • ❏ ${prefix}info
+  • ❏ ${prefix}blocklist
+  • ❏ ${prefix}chatlist
+  • ❏ ${prefix}ping
+  • ❏ ${prefix}bugreport
 ◪ *GROUP*
-  │
-  ├─ ❏ ${prefix}opengc
-  ├─ ❏ ${prefix}closegc
-  ├─ ❏ ${prefix}promote
-  ├─ ❏ ${prefix}demote
-  ├─ ❏ ${prefix}tagall
-  ├─ ❏ ${prefix}tagall2
-  ├─ ❏ ${prefix}tagall3
-  ├─ ❏ ${prefix}tagall4
-  ├─ ❏ ${prefix}tagall5
-  ├─ ❏ ${prefix}add
-  ├─ ❏ ${prefix}kick
-  ├─ ❏ ${prefix}listadmins
-  ├─ ❏ ${prefix}linkgroup
-  ├─ ❏ ${prefix}leave
-  ├─ ❏ ${prefix}welcome
-  ├─ ❏ ${prefix}nsfw
-  ├─ ❏ ${prefix}leveling
-  ├─ ❏ ${prefix}level
-  ├─ ❏ ${prefix}delete
-  ├─ ❏ ${prefix}simih
-  └─ ❏ ${prefix}ownergroup`
+  • ❏ ${prefix}opengc
+  • ❏ ${prefix}closegc
+  • ❏ ${prefix}promote
+  • ❏ ${prefix}demote
+  • ❏ ${prefix}tagall
+  • ❏ ${prefix}tagall2
+  • ❏ ${prefix}tagall3
+  • ❏ ${prefix}tagall4
+  • ❏ ${prefix}tagall5
+  • ❏ ${prefix}add
+  • ❏ ${prefix}kick
+  • ❏ ${prefix}listadmins
+  • ❏ ${prefix}linkgroup
+  • ❏ ${prefix}leave
+  • ❏ ${prefix}welcome
+  • ❏ ${prefix}nsfw
+  • ❏ ${prefix}leveling
+  • ❏ ${prefix}level
+  • ❏ ${prefix}delete
+  • ❏ ${prefix}simih
+  • ❏ ${prefix}ownergroup`
 }
 exports.group = group
