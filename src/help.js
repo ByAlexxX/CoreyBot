811d9ac35e62c56438eb0ab502d484199fe25ee6
ByAlexxX <92841381+ByAlexxX@users.noreply.github.com>
2021-10-22 15:47:11 -0500

    Update help.js

--- src/help.js
+++ src/help.js
@@ -11,90 +11,90 @@ const help = (prefix) => {
 Registrate con el comando ${prefix}daftar y tu nombre
 
 ◉ *NUEVOS COMANDOS*
-○ ${prefix}banmenu
-Banear a personas el uso del bot
-○ ${prefix}levelmenu
-Level para tus grupos
-○ ${prefix}antimenu
-Su nombre lo dice todo (antilink)
-○ ${prefix}otak
-Monas chinas
-○ ${prefix}shantera
-Interactua con el bot
+ ○ ${prefix}banmenu
+  Banear a personas el uso del bot
+ ○ ${prefix}levelmenu
+  Level para tus grupos
+ ○ ${prefix}antimenu
+  Su nombre lo dice todo (antilink)
+ ○ ${prefix}otak
+  Monas chinas
+ ○ ${prefix}shantera
+  Interactua con el bot
 
 ◉ *NUEVOS MENUS*
-  ○ ${prefix}juegos
+ ○ ${prefix}juegos
   Diviértete con tus amigos :)
-  ○ ${prefix}desmenu
+ ○ ${prefix}desmenu
   Descargar musica y videos
-  ○ ${prefix}version
+ ○ ${prefix}version
   Conoce la versión de tu bot
-  ○ ${prefix}welmenu
+ ○ ${prefix}welmenu
   Comando de bienvenida solo grupos
 
 ◉ *RESUELVE TUS DUDAS*
 
-  ○ ${prefix}creador
+ ○ ${prefix}creador
   Dudas o problemas aqui
 
 ◉ *CREAR STICKERS*
 
-  ○ ${prefix}sticker
-  ○ ${prefix}attp
+ ○ ${prefix}sticker
+ ○ ${prefix}attp
    Mas un texto corto
-  ○ ${prefix}stickergif
+ ○ ${prefix}stickergif
   6 segundos de video
 
 ◉ *CONVERTIDORES*
 
-  ○ ${prefix}toimg
-  De sticker a JPG
-  ○ ${prefix}tomp3
+ ○ ${prefix}toimg
+  De sticker a JPG 
+ ○ ${prefix}tomp3
   De video a MP3
 
 ◉ *AUDIO*
 
-  ○ ${prefix}idioma
-  ○ ${prefix}tts es (mas texto)
+ ○ ${prefix}idioma
+ ○ ${prefix}tts es (mas texto)
 
 ◉ *OTROS*
 
-  ○ ${prefix}wame
+ ○ ${prefix}wame
   Link de Whatsapp
-  ○ ${prefix}qrcode
+ ○ ${prefix}qrcode
   Coloca un texto
 
 ◉ *GRUPOS*
 
-  ○ ${prefix}unir
+ ○ ${prefix}unir
   Unir a una persona al grupo
-  ○ ${prefix}fgc
+ ○ ${prefix}fgc
   Cambiar foto del grupo
-  ○ ${prefix}ngc
+ ○ ${prefix}ngc
   Cambiar nombre del grupo
-  ${prefix}dgc
+ ○ ${prefix}dgc
   Cambiar descripcion del grupo
-  ○ ${prefix}closegc
+ ○ ${prefix}closegc
   Cerrar el grupo solo admins
-  ○ ${prefix}opengc
+ ○ ${prefix}opengc
   Abrir grupo solo admins
-  ○ ${prefix}kick
+ ○ ${prefix}kick
   Eliminar a un miembro 
-  ○ ${prefix}promote
+ ○ ${prefix}promote
   Dar admin a un miembro
-  ○ ${prefix}demote
+ ○ ${prefix}demote
   Quitar el admin
-  ○ ${prefix}linkgc
+ ○ ${prefix}linkgc
   Link del grupo
-  ○ ${prefix}adminlist
+ ○ ${prefix}adminlist
   Nombra a los admins del grupo
-  ○ ${prefix}todos
+ ○ ${prefix}todos
   Nombra a todos los del grupo
 
 Para usar estas funciones el bot necesita admin
   
 ◉ *NSWF* 
-  • ${prefix}nsfwmenu
+ • ${prefix}nsfwmenu
 
 Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
 
