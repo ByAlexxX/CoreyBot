0954f1de927013fd82ad5b63050e7bea1dda61a9
ByAlexxX <92841381+ByAlexxX@users.noreply.github.com>
2021-10-22 14:54:05 -0500

    Update wibu.js

--- database/menu/wibu.js
+++ database/menu/wibu.js
@@ -6,22 +6,20 @@ const wibu = (prefix, ownerBot, botName) => {
   ❏ Prefix: 「  ${prefix}  」
   ❏ Creator: ${ownerBot}
 ◪ *ABOUT*
-  │
-  ├─ ❏ ${prefix}info
-  ├─ ❏ ${prefix}blocklist
-  ├─ ❏ ${prefix}chatlist
-  ├─ ❏ ${prefix}ping
-  └─ ❏ ${prefix}bugreport
+  • ❏ ${prefix}info
+  • ❏ ${prefix}blocklist
+  • ❏ ${prefix}chatlist
+  • ❏ ${prefix}ping
+  • ❏ ${prefix}bugreport
 ◪ *WIBU*
-  │
-  ├─ ❏ ${prefix}neonime
-  ├─ ❏ ${prefix}pokemon
-  ├─ ❏ ${prefix}loli
-  ├─ ❏ ${prefix}waifu
-  ├─ ❏ ${prefix}randomanime
-  ├─ ❏ ${prefix}husbu
-  ├─ ❏ ${prefix}husbu2
-  ├─ ❏ ${prefix}wait
-  └─ ❏ ${prefix}nekonime`
+  • ❏ ${prefix}neonime
+  • ❏ ${prefix}pokemon
+  • ❏ ${prefix}loli
+  • ❏ ${prefix}waifu
+  • ❏ ${prefix}randomanime
+  • ❏ ${prefix}husbu
+  • ❏ ${prefix}husbu2
+  • ❏ ${prefix}wait
+  • ❏ ${prefix}nekonime`
 }
 exports.wibu = wibu
