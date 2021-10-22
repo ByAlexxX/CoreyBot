9146b891fcb8a02533a096618b6cd1ac8591b143
ByAlexxX <92841381+ByAlexxX@users.noreply.github.com>
2021-10-22 15:03:24 -0500

    Update music.js

--- database/menu/music.js
+++ database/menu/music.js
@@ -6,17 +6,15 @@ const music = (prefix, ownerBot, botName) => {
   ❏ Prefix: 「  ${prefix}  」
   ❏ Creator: ${ownerBot}
 ◪ *ABOUT*
-  │
-  ├─ ❏ ${prefix}info
-  ├─ ❏ ${prefix}blocklist
-  ├─ ❏ ${prefix}chatlist
-  ├─ ❏ ${prefix}ping
-  └─ ❏ ${prefix}bugreport
+  • ❏ ${prefix}info
+  • ❏ ${prefix}blocklist
+  • ❏ ${prefix}chatlist
+  • ❏ ${prefix}ping
+  • ❏ ${prefix}bugreport
 ◪ *MUSIC*
-  │
-  ├─ ❏ ${prefix}play
-  ├─ ❏ ${prefix}joox
-  ├─ ❏ ${prefix}lirik
-  └─ ❏ ${prefix}chord`
+  • ❏ ${prefix}play
+  • ❏ ${prefix}joox
+  • ❏ ${prefix}lirik
+  • ❏ ${prefix}chord`
 }
 exports.music = music
