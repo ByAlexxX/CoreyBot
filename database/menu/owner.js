56657b04cfe90c2ca5122ae68a184c0ba1e138e1
ByAlexxX <92841381+ByAlexxX@users.noreply.github.com>
2021-10-22 15:01:24 -0500

    Update owner.js

--- database/menu/owner.js
+++ database/menu/owner.js
@@ -6,19 +6,17 @@ const owner = (prefix, ownerBot, botName) => {
   ❏ Prefix: 「  ${prefix}  」
   ❏ Creator: ${ownerBot}
 ◪ *ABOUT*
-  │
-  ├─ ❏ ${prefix}info
-  ├─ ❏ ${prefix}blocklist
-  ├─ ❏ ${prefix}chatlist
-  ├─ ❏ ${prefix}ping
-  └─ ❏ ${prefix}bugreport
+  • ❏ ${prefix}info
+  • ❏ ${prefix}blocklist
+  • ❏ ${prefix}chatlist
+  • ❏ ${prefix}ping
+  • ❏ ${prefix}bugreport
 ◪ *OWNER*
-  │
-  ├─ ❏ ${prefix}setprefix
-  ├─ ❏ ${prefix}block
-  ├─ ❏ ${prefix}bc
-  ├─ ❏ ${prefix}bcgc
-  ├─ ❏ ${prefix}clone
-  └─ ❏ ${prefix}clearall`
+  • ❏ ${prefix}setprefix
+  • ❏ ${prefix}block
+  • ❏ ${prefix}bc
+  • ❏ ${prefix}bcgc
+  • ❏ ${prefix}clone
+  • ❏ ${prefix}clearall`
 }
 exports.owner = owner
